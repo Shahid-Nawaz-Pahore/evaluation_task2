0facb387c56b078073043a910ec0bc89e1bbfe4e
Shahid Nawaz <shahidnawaz.edu.pk@gmail.com>
2024-09-11 12:26:28 -0700

    octa erc721 is compile successfully

--- README.md
+++ README.md
@@ -1,13 +1,4 @@
-# Sample Hardhat Project
+``````````````````````OCTA ERC20 Token address`````````````````````
+ https://sepolia.etherscan.io/address/0x2880b2025c109624aC84e245214792Be4F36B446
 
-This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a Hardhat Ignition module that deploys that contract.
-
-Try running some of the following tasks:
-
-```shell
-npx hardhat help
-npx hardhat test
-REPORT_GAS=true npx hardhat test
-npx hardhat node
-npx hardhat ignition deploy ./ignition/modules/Lock.js
-```
+ 
---
+++ contracts/octaERC721.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract OCTA_ERC721_Token is ERC721{
+    uint256 private _nextTokenId;
+    address public owner;
+    constructor() ERC721("OCTA_ERC721_Token", "OCTA721"){
+        owner=msg.sender;
+    }
+    modifier onlyOwner{
+        require(msg.sender==owner, "Only owner can call this function");
+        _;
+    }
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+    }
+}
